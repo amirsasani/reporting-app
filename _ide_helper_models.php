b2eec416c5c287e5fa5a3ccbb8e0d9bc1f95dd9f
Amir Sasani <sasani.amir.1998@gmail.com>
2023-07-30 02:58:14 +0330

    add ide helper

---
+++ _ide_helper_models.php
@@ -0,0 +1,62 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Field
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Field newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Field newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Field query()
+ */
+	class Field extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Province
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Province newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Province newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Province query()
+ */
+	class Province extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Report
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report query()
+ */
+	class Report extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property mixed $password
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ */
+	class User extends \Eloquent {}
+}
+
